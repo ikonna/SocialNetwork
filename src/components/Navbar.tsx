afca4dea2b0dd8624378ba9ba36decea0b5a3b21
user <user@users-iMac.local>
2020-12-28 15:19:16 -0800

    SN

---
+++ src/components/Navbar.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+
+const Navbar = () => {
+    return   <nav className='navigation'>
+        <div>Profile</div>
+        <div>Messages</div>
+        <div>Settings</div>
+        <div>Photo</div>
+    </nav>
+}
+
+export default Navbar;
---
+++ src/components/Profile.tsx
@@ -0,0 +1,26 @@
+import React from 'react';
+
+
+const Profile = () => {
+    return  <div className='content'>
+        <div>
+            <img
+                src='https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRbZTPBifRk4nLz9dLqgKsJxFIkt8HgzGfqNQ&usqp=CAU'/>
+        </div>
+        <div>
+            <div>
+                <img src='https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQdUOfpis-6LPzE7ICUB5wwhqktW4g2yyy9Dw&usqp=CAU'/>
+            </div>
+
+            <div>
+                <div>Kirill Ilon</div>
+                <div>23 jii</div>
+                <div>student</div>
+                <div>Barista</div>
+                <div>Himout</div>
+            </div>
+        </div>
+    </div>
+}
+
+export default Profile;
