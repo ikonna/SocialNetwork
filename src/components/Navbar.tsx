e9d3385a43b318a59cd11be1aedcad929a01a811
ikonna <76710476+ikonna@users.noreply.github.com>
2021-01-06 15:51:57 -0800

    my first commit

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,13 +1,29 @@
 import React from 'react';
+import s from './Navbar.module.css'
 
-
+//let s = {
+//    'nav': 'Navbar_navigation__gaxyV',
+//    'item': 'Navbar_item__1Raqz'
+//}
 const Navbar = () => {
-    return   <nav className='navigation'>
-        <div>Profile</div>
-        <div>Messages</div>
-        <div>Settings</div>
-        <div>Photo</div>
+    return <nav className={s.nav}>
+        <div className={s.item}>
+            <a>Profile</a>
+        </div>
+        <div className={s.item}>
+            <a>Messages</a>
+        </div>
+        <div className={s.item}>
+            <a>Settings</a>
+        </div>
+        <div className={s.item}>
+            <a>Photo</a>
+        </div>
     </nav>
 }
 
-export default Navbar;
+export default Navbar
+
+
+
+
--- src/components/Profile.tsx
+++ src/components/Profile.tsx
@@ -1,23 +1,34 @@
 import React from 'react';
-
+import s from './Profile.module.css';
 
 const Profile = () => {
-    return  <div className='content'>
+    return <div className={s.content}>
         <div>
             <img
                 src='https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRbZTPBifRk4nLz9dLqgKsJxFIkt8HgzGfqNQ&usqp=CAU'/>
         </div>
         <div>
             <div>
-                <img src='https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQdUOfpis-6LPzE7ICUB5wwhqktW4g2yyy9Dw&usqp=CAU'/>
+                <img
+                    src='https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQdUOfpis-6LPzE7ICUB5wwhqktW4g2yyy9Dw&usqp=CAU'/>
             </div>
 
-            <div>
-                <div>Kirill Ilon</div>
-                <div>23 jii</div>
-                <div>student</div>
-                <div>Barista</div>
-                <div>Himout</div>
+            <div className={s.posts}>
+                <div className={s.item}>
+                    ill Ilon
+                </div>
+                <div className={s.item}>
+                    23 jii
+                </div>
+                <div className={s.item}>
+                    student
+                </div>
+                <div className={s.item}>
+                    Barista
+                </div>
+                <div className={s.item}>
+                    Himout
+                </div>
             </div>
         </div>
     </div>
